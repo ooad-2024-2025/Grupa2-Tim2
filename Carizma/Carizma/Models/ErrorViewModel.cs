66ed8438a5c35577ab4b7adce3e9f2571b90734e
lgicevic1 <lgicevic1@etf.unsa.ba>
2025-04-15 11:43:06 +0200

    Commit

---
+++ Carizma/Carizma/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Carizma.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
